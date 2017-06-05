a5bea3d6bede00b0a6769e9d7116964a85a57786
shivsundram <shivsundram@gmail.com>
2017-06-05 01:43:51 +0000

    destroying the val set

--- enhanced_training.py
+++ enhanced_training.py
@@ -27,15 +27,15 @@ def get_CIFAR10_data(source, num_training=49000, num_validation=1000, num_test=1
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # Subsample the data
-    mask = range(num_training, num_training + num_validation)
-    X_val = X_train[mask]
-    y_val = y_train[mask]
-    mask = range(num_training)
-    X_train = X_train[mask]
-    y_train = y_train[mask]
-    mask = range(num_test)
-    X_test = X_test[mask]
-    y_test = y_test[mask]
+    #mask = range(num_training, num_training + num_validation)
+    #X_val = X_train[mask]
+    #y_val = y_train[mask]
+    #mask = range(num_training)
+    #X_train = X_train[mask]
+    #y_train = y_train[mask]
+    #mask = range(num_test)
+    #X_test = X_test[mask]
+    #y_test = y_test[mask]
 
     # Normalize the data: subtract the mean image
     #mean_image = np.mean(X_train, axis=0)
@@ -43,7 +43,7 @@ def get_CIFAR10_data(source, num_training=49000, num_validation=1000, num_test=1
     #X_val -= mean_image
     #X_test -= mean_image
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    return X_train, y_train, X_test, y_test
 
 
 if __name__ == '__main__':
@@ -82,11 +82,9 @@ if __name__ == '__main__':
         print("need to specity --use-train or --use-test")
         sys.exit()    
     #get cifar10
-    X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data(source)
+    X_train, y_train, X_test, y_test = get_CIFAR10_data(source)
     print('Train data shape: ', X_train.shape)
     print('Train labels shape: ', y_train.shape)
-    print('Validation data shape: ', X_val.shape)
-    print('Validation labels shape: ', y_val.shape)
     print('Test data shape: ', X_test.shape)
     print('Test labels shape: ', y_test.shape)
 
